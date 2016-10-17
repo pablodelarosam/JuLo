4cc85d103591615285af66b9715f5a4cd35474cb
pablodelarosam <pablo.delarosaml@udlap.mx>
2016-10-17 12:19:34 -0500

    Initial Commit

---
+++ MathsInvader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MathsInvader
+//
+//  Created by Pablo de la Rosa Michicol on 10/17/16.
+//  Copyright © 2016 Juan Carlos Ramírez Cabrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
